24911442c6f00033cb666e33ba8a82c3e723dd59
Patch <patchandthat@gmail.com>
2015-11-12 19:32:36 +0000

    lesson 1.2

---
+++ Unit 1/Messages.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTail
+{
+    class Messages
+    {
+        #region Neutral/system messages
+        public class ContinueProcessing { }
+        #endregion
+
+        #region Success messages
+
+        public class InputSuccess
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+            /// </summary>
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+        #endregion
+
+        #region Error messages
+        /// <summary>
+        /// Base class for signalling that user input was invalid.
+        /// </summary>
+        public class InputError
+        {
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+
+        /// <summary>
+        /// User provided blank input.
+        /// </summary>
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason) { }
+        }
+
+        /// <summary>
+        /// User provided invalid input (currently, input w/ odd # chars)
+        /// </summary>
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason) { }
+        }
+        #endregion
+    }
+}
