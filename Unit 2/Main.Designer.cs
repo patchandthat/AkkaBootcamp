c5528567a3a1bde563063b06471d32ac76e23ae3
Patch <patchandthat@gmail.com>
2015-11-13 12:27:00 +0000

    2.2

--- Unit 2/Main.Designer.cs
+++ Unit 2/Main.Designer.cs
@@ -32,6 +32,7 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.sysChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.addSeriesButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.sysChart)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,11 +53,22 @@
             this.sysChart.TabIndex = 0;
             this.sysChart.Text = "sysChart";
             // 
+            // addSeriesButton
+            // 
+            this.addSeriesButton.Location = new System.Drawing.Point(564, 353);
+            this.addSeriesButton.Name = "addSeriesButton";
+            this.addSeriesButton.Size = new System.Drawing.Size(108, 42);
+            this.addSeriesButton.TabIndex = 1;
+            this.addSeriesButton.Text = "Add Series";
+            this.addSeriesButton.UseVisualStyleBackColor = true;
+            this.addSeriesButton.Click += new System.EventHandler(this.addSeriesButton_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 446);
+            this.Controls.Add(this.addSeriesButton);
             this.Controls.Add(this.sysChart);
             this.Name = "Main";
             this.Text = "System Metrics";
@@ -70,6 +82,7 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart sysChart;
+        private System.Windows.Forms.Button addSeriesButton;
     }
 }
 
